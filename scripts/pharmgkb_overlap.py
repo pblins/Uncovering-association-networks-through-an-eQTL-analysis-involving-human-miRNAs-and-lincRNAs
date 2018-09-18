4b9fda0e27fa282ea946c13cfed7d246da5bc080
Paulo Branco <paulorobertobranco@gmail.com>
2018-09-18 11:49:50 -0300

    Update pharmgkb_overlap.py

--- scripts/pharmgkb_overlap.py
+++ scripts/pharmgkb_overlap.py
@@ -5,14 +5,14 @@ from os import listdir
 from os.path import isfile, join
 
 ######################## OUTLINE #########################
-# THIS SCRIPT OVELAPS THE SIGNIFICANT SNPS WITH 		 # 
-# PHARMGKB DATA											 #
+# THIS SCRIPT OVELAPS THE SIGNIFICANT SNPS WITH		  # 
+# PHARMGKB DATA						  #
 ##########################################################
 
 ######################## PARAMS ##########################
-# INPUT 1: PHARMGKB DATA FILE 							 #
-# INPUT 2: SIGNIFICANT SNPS FILE						 #
-# OUTPUT: OVERLAPPING RESULT FILE 						 #
+# INPUT 1: PHARMGKB DATA FILE				  #
+# INPUT 2: SIGNIFICANT SNPS FILE			  #
+# OUTPUT: OVERLAPPING RESULT FILE			  #
 ##########################################################
 
 PHARGKB_FILE = sys.argv[1]
